be680d6e49108db216c894ea07dd49c88fabddcf
Tom Leonardsson <tom.leo@hotmail.com>
2017-01-20 21:02:18 +0100

    basic gameobject

--- core/src/se/snittarna/eddington/GameObject.java
+++ core/src/se/snittarna/eddington/GameObject.java
@@ -1,5 +1,28 @@
 package se.snittarna.eddington;
 
-public class GameObject {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
+public abstract class GameObject {
+	private Vector2 position;
+	
+	public void update() {
+		
+	}
+	
+	public void draw(SpriteBatch batch) {
+		
+	}
+	
+	public void drawUi(SpriteBatch batch) {
+		
+	}
+	
+	public Vector2 getPosition() {
+		return position.cpy();
+	}
+	
+	public void setPosition(Vector2 position) {
+		this.position = position.cpy();
+	}
 }
