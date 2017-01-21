fa5ceb14133aa81cab934ae0a0e3e9bb8fe8b87e
Tom Leonardsson <tom.leo@hotmail.com>
2017-01-21 11:27:00 +0100

    added projectile class

--- core/src/se/snittarna/eddington/Projectile.java
+++ core/src/se/snittarna/eddington/Projectile.java
@@ -3,11 +3,22 @@ package se.snittarna.eddington;
 import com.badlogic.gdx.math.Vector2;
 
 public class Projectile extends GameObject {
-	public Projectile(Vector2 position, Vector2 size, Animation sprite) {
+	private float angle;
+	private float speed;
+	private float fallSpeed;
+	
+	public Projectile(Vector2 position, Vector2 size, Animation sprite, float angle, float speed) {
 		super(position, size, sprite);
+		this.angle = angle;
+		this.speed = speed;
 	}
 	
 	public void update(float dt) {
-		
+		this.setPosition(new Vector2(this.getPosition().cpy().x + getVelocity().cpy().x*dt, this.getPosition().cpy().y + getVelocity().cpy().y*dt));
+		fallSpeed += 1 * dt;
+	}
+	
+	public Vector2 getVelocity() {
+		return new Vector2((float)Math.cos(angle)*speed, (float)Math.sin(angle)*speed);
 	}
 }
