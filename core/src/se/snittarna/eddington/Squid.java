a7b89dc76545ad90394bece49940862e114a389d
Tom Leonardsson <tom.leo@hotmail.com>
2017-01-21 13:13:54 +0100

    did squid stuff

--- core/src/se/snittarna/eddington/Squid.java
+++ core/src/se/snittarna/eddington/Squid.java
@@ -3,7 +3,24 @@ package se.snittarna.eddington;
 import com.badlogic.gdx.math.Vector2;
 
 public class Squid extends GameObject {
+	private float attackCount;
+	private float maxAttackCount;
+	
+	private int armToUseIndex;
+	
+	private boolean hasAttackTeased;
+	
 	public Squid(Vector2 position, Vector2 size, Animation sprite) {
 		super(position, size, sprite);
+	}
+	
+	public void update(float dt) {
+		super.update(dt);
+	}
+	
+	public void attack() {
+		if(!hasAttackTeased) {
+			
+		}
 	}
 }
--- core/src/se/snittarna/eddington/SquidArm.java
+++ core/src/se/snittarna/eddington/SquidArm.java
@@ -1,5 +1,8 @@
 package se.snittarna.eddington;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class SquidArm extends GameObject {
@@ -8,7 +11,10 @@ public class SquidArm extends GameObject {
 	private float currentLevel;
 	private float extendSpeed;
 	
+	private final float BASE_LEVEL = 1;
 	private final float MAX_EXTENSION = 100f;
+	
+	private Sprite arm;
 	
 	public SquidArm(Vector2 position, Vector2 size, Animation sprite) {
 		super(position, size, sprite);
@@ -20,5 +26,17 @@ public class SquidArm extends GameObject {
 	
 	public void extend() {
 		currentLevel += extendSpeed;
+	}
+	
+	public void tease() {
+		float highTease = BASE_LEVEL + 0.5f;
+		float lowTease = BASE_LEVEL + 0.5f;
+		
+		//MathUtils.lerp(fromValue, toValue, progress);
+	}
+	
+	public void draw(SpriteBatch batch) {
+		super.draw(batch);
+		if(arm != null) arm.draw(batch);
 	}
 }
