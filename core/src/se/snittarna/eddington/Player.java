42908b4e2988e4c2918624260f2dd0b6685d4f15
Johannes Larsson <developer.johannes.larsson@gmail.com>
2017-01-22 13:16:06 +0100

    player

--- core/src/se/snittarna/eddington/Player.java
+++ core/src/se/snittarna/eddington/Player.java
@@ -130,11 +130,11 @@ public class Player extends GameObject implements Depthable {
 			/** 
 			 * move left and right
 			 */
-			if (Gdx.input.isKeyPressed(Keys.A) && getPosition().x > (-330/2)+20) {
+			if (Gdx.input.isKeyPressed(Keys.A) && getPosition().x > (-330/2)+50 || getPosition().x > 145) {
 				velocity.x -= ACCELERATION * dt;
 			}
 			
-			if (Gdx.input.isKeyPressed(Keys.D) && getPosition().x < (330/2)-20) {
+			if (Gdx.input.isKeyPressed(Keys.D) && getPosition().x < (330/2)-50 || getPosition().x < -145) {
 				velocity.x += ACCELERATION * dt;
 			}
 		}
