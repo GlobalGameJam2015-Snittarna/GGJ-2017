227b62f3003c99467156d88ad9357801105a0e10
Tom Leonardsson <tom.leo@hotmail.com>
2017-01-22 12:57:24 +0100

    added underwater naimatin

--- core/src/se/snittarna/eddington/Underwater.java
+++ core/src/se/snittarna/eddington/Underwater.java
@@ -6,7 +6,12 @@ import com.badlogic.gdx.math.Vector2;
 public class Underwater extends GameObject {
 
 	public Underwater() {
-		super(new Vector2(-165, -90), new Vector2(330, 49), new Animation(new Sprite(AssetManager.getTexture("underwater"))));
+		super(new Vector2(-165, -90), new Vector2(330, 49), new Animation(new Sprite(AssetManager.getTexture("underwater")), 2, 0, 4));
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void update(float dt) {
+		getSprite().animate(dt);
+		super.update(dt);
 	}
 }
