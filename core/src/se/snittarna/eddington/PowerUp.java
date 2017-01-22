82085d0008a9204227e019bd31ff68c19cb44c9a
Tom Leonardsson <tom.leo@hotmail.com>
2017-01-22 02:35:44 +0100

    added rectangle ands tareteads on pwoersoisps

--- core/src/se/snittarna/eddington/PowerUp.java
+++ core/src/se/snittarna/eddington/PowerUp.java
@@ -1,15 +1,18 @@
 package se.snittarna.eddington;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public class PowerUp extends GameObject {
 	public enum Type {BARREL, BOMB};
 	
-	public PowerUp(Vector2 position, Vector2 size, Animation sprite) {
-		super(position, size, sprite);
+	private Type type;
+	
+	public PowerUp(Vector2 position, Type type) {
+		super(position, new Vector2(32, 32),  new Animation(new Sprite(AssetManager.getTexture(getImageName(type)))));
 	}
 	
-	public String getImageName(Type type) {
+	public static String getImageName(Type type) {
 		return (type == Type.BARREL) ? "barrel" : "bomb";
 	}
 }
